2356918c5e8d49cbc978cae722245455014b19da
brent <brent.gerets@gmail.com>
2022-12-03 09:21:13 +0100

    day 3 part 2 completed

--- day3/main.py
+++ day3/main.py
@@ -2,13 +2,14 @@ if __name__ == '__main__':
     inputFile = open("input.txt", "r")
 
     total = 0
+    group = []
     for line in inputFile:
-        length = len(line) - 1
-        comp1 = line[:int(length / 2)]
-        comp2 = line[int(length / 2):]
-        common = list(set(comp1).intersection(comp2))[0]
-        value = ord(common) - 96 if common.islower() else ord(common) - 38
-        total += value
+        group.append(line[:len(line) - 1])
+        if len(group) == 3:
+            common = list(set(group[0]).intersection(group[1], group[2]))[0]
+            value = ord(common) - 96 if common.islower() else ord(common) - 38
+            total += value
+            group.clear()
     print(total)
 
     inputFile.close()
