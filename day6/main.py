6f687512ab192a9cdf6a5a22be95ba9c7f071769
Brent Gerets <brent.gerets@student.uhasselt.be>
2022-12-06 08:47:42 +0100

    day 6 part 1 completed

---
+++ day6/main.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    inputFile = open("input.txt", "r")
+
+    marker = []
+    index = 1
+    for char in inputFile.read():
+        marker.append(char)
+        if len(marker) < 4:
+            index += 1
+            continue
+        if len(marker) > 4:
+            marker.pop(0)
+        if len(marker) == len(set(marker)):
+            break
+        index += 1
+    print(index)
+    inputFile.close()
