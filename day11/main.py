342089cac6a8afeffcb137149f762dc86698900e
brent <brent.gerets@gmail.com>
2022-12-11 13:57:10 +0100

    day 11 part 2 completed

--- day11/main.py
+++ day11/main.py
@@ -1,4 +1,4 @@
-def inspect(item, operation):
+def inspect(item, operation, mod):
     if operation[6:] == "old":
         nr = item
     else:
@@ -7,6 +7,8 @@ def inspect(item, operation):
         item *= nr
     elif operation[4] == '+':
         item += nr
+    if item >= mod:
+        item = item % mod
     return item
 
 
@@ -18,6 +20,7 @@ if __name__ == '__main__':
     monkeyTest = []
     monkeyThrow = []
     monkeyInspect = []
+    mod = 1
     monkeyIdx = 0
     while True:
         inputFile.readline()
@@ -39,6 +42,7 @@ if __name__ == '__main__':
         # read test
         line = inputFile.readline()
         monkeyTest.append(int(line[line.find("by") + 3:]))
+        mod *= monkeyTest[monkeyIdx]
         # read throw
         monkeyThrow.append([])
         line = inputFile.readline()
@@ -50,24 +54,21 @@ if __name__ == '__main__':
             break
 
     roundNr = 1
-    while roundNr <= 20:
-        print(">>> ROUND " + str(roundNr))
+    while roundNr <= 10000:
+        # print(">>> ROUND " + str(roundNr))
         for monkeyIdx in range(len(monkeyItems)):
-            print("\tmonkey " + str(monkeyIdx) + ":")
+            # print("\tmonkey " + str(monkeyIdx) + ":")
             while len(monkeyItems[monkeyIdx]) > 0:
                 monkeyInspect[monkeyIdx] += 1
-                print("\t\tmonkey inspects item with worry level " + str(monkeyItems[monkeyIdx][0]))
-                monkeyItems[monkeyIdx][0] = inspect(monkeyItems[monkeyIdx][0], monkeyOps[monkeyIdx])
-                print("\t\t\tworry level is now " + str(monkeyItems[monkeyIdx][0]))
-                monkeyItems[monkeyIdx][0] = int(monkeyItems[monkeyIdx][0] / 3)
-                print("\t\t\tworry level divided by 3: " + str(monkeyItems[monkeyIdx][0]))
+                # print("\t\tmonkey inspects item with worry level " + str(monkeyItems[monkeyIdx][0]))
+                monkeyItems[monkeyIdx][0] = inspect(monkeyItems[monkeyIdx][0], monkeyOps[monkeyIdx], mod)
+                # print("\t\t\tworry level is now " + str(monkeyItems[monkeyIdx][0]))
                 newMonkeyIdx = monkeyThrow[monkeyIdx][0] if monkeyItems[monkeyIdx][0] % monkeyTest[monkeyIdx] == 0 else monkeyThrow[monkeyIdx][1]
-                print("\t\t\titem thrown to monkey " + str(newMonkeyIdx))
+                # print("\t\t\titem thrown to monkey " + str(newMonkeyIdx))
                 monkeyItems[newMonkeyIdx].append(monkeyItems[monkeyIdx][0])
                 monkeyItems[monkeyIdx].pop(0)
         roundNr += 1
-        print()
-    print(monkeyInspect)
+        # print()
     monkeyInspect.sort(reverse=True)
     print(monkeyInspect[0] * monkeyInspect[1])
 
