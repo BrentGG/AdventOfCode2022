235b1b14d5afdde9296990cb5a628f5c10a1d181
brent <brent.gerets@gmail.com>
2022-12-13 18:40:26 +0100

    day 12 part 1 completed

--- day12/main.py
+++ day12/main.py
@@ -1,37 +1,59 @@
-import time
+def searchMin(distance, unvisited):
+    minPos = unvisited[0]
+    for pos in unvisited:
+        if distance[pos[0]][pos[1]] < distance[minPos[0]][minPos[1]]:
+            minPos = pos
+    return minPos
 
 
-def isPossibleMove(heightMap, moveTo, moveFrom, covered):
-    if moveTo[0] < 0 or moveTo[0] >= len(heightMap) or moveTo[1] < 0 or moveTo[1] >= len(heightMap[0]):
+def isNeighbour(heightMap, neighbour, pos, unvisited):
+    if neighbour not in unvisited:
         return False
-    if moveTo in covered:
+    if neighbour[0] < 0 or neighbour[0] >= len(heightMap) or neighbour[1] < 0 or neighbour[1] >= len(heightMap[0]):
         return False
-    if heightMap[moveTo[0]][moveTo[1]] > heightMap[moveFrom[0]][moveFrom[1]] + 1:
+    if heightMap[neighbour[0]][neighbour[1]] > heightMap[pos[0]][pos[1]] + 1:
         return False
     return True
 
 
-def pathFind(heightMap, end, pos, steps, covered):
-    covered.append(pos)
-    if pos == end:
-        return steps
-    else:
-        values = []
-        if isPossibleMove(heightMap, [pos[0] - 1, pos[1]], pos, covered):  # move up
-            values.append(pathFind(heightMap, end, [pos[0] - 1, pos[1]], steps + 1, covered.copy()))
-        if isPossibleMove(heightMap, [pos[0] + 1, pos[1]], pos, covered):  # move down
-            values.append(pathFind(heightMap, end, [pos[0] + 1, pos[1]], steps + 1, covered.copy()))
-        if isPossibleMove(heightMap, [pos[0], pos[1] - 1], pos, covered):  # move left
-            values.append(pathFind(heightMap, end, [pos[0], pos[1] - 1], steps + 1, covered.copy()))
-        if isPossibleMove(heightMap, [pos[0], pos[1] + 1], pos, covered):  # move right
-            values.append(pathFind(heightMap, end, [pos[0], pos[1] + 1], steps + 1, covered.copy()))
-        if -1 in values:
-            values.remove(-1)
-        if len(values) == 0:
-            return -1
-        else:
-            values.sort()
-            return values[0]
+def getNeighbours(heightMap, pos, unvisited):
+    neighbours = []
+    if isNeighbour(heightMap, [pos[0] - 1, pos[1]], pos, unvisited):  # move up
+        neighbours.append([pos[0] - 1, pos[1]])
+    if isNeighbour(heightMap, [pos[0] + 1, pos[1]], pos, unvisited):  # move down
+        neighbours.append([pos[0] + 1, pos[1]])
+    if isNeighbour(heightMap, [pos[0], pos[1] - 1], pos, unvisited):  # move left
+        neighbours.append([pos[0], pos[1] - 1])
+    if isNeighbour(heightMap, [pos[0], pos[1] + 1], pos, unvisited):  # move right
+        neighbours.append([pos[0], pos[1] + 1])
+    return neighbours
+
+
+def dijkstra(heightMap, source, target):
+    distance = []
+    previous = []
+    unvisited = []
+    for i in range(len(heightMap)):
+        distance.append([])
+        previous.append([])
+        for j in range(len(heightMap[0])):
+            distance[i].append(999999)
+            previous[i].append(None)
+            unvisited.append([i, j])
+    distance[source[0]][source[1]] = 0
+
+    while len(unvisited) > 0:
+        current = searchMin(distance, unvisited)
+        if current == target:
+            break
+        unvisited.remove(current)
+        for neighbour in getNeighbours(heightMap, current, unvisited):
+            newDist = distance[current[0]][current[1]] + 1
+            if newDist < distance[neighbour[0]][neighbour[1]]:
+                distance[neighbour[0]][neighbour[1]] = newDist
+                previous[neighbour[0]][neighbour[1]] = current
+
+    return distance, previous
 
 
 if __name__ == '__main__':
@@ -56,8 +78,7 @@ if __name__ == '__main__':
             col += 1
         row += 1
 
-    startTime = time.time()
-    print(pathFind(heightMap, end, start, 0, []))
-    print("Execution took " + str(time.time() - startTime) + " seconds.")
+    dist, prev = dijkstra(heightMap, start, end)
+    print(dist[end[0]][end[1]])
 
     inputFile.close()
