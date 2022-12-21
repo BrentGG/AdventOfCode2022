62d7932c270dc2a4e914481f10d4a62f9e1eba7e
Brent Gerets <brent.gerets@student.uhasselt.be>
2022-12-21 09:59:06 +0100

    day 21 part 2 completed

--- day21/main.py
+++ day21/main.py
@@ -1,22 +1,22 @@
-def resolve(name, names, content, operations):
+def resolve(name, names, cont, ops):
     idx = names.index(name)
-    if operations[idx] == '':
-        return content[idx]
+    if ops[idx] == '':
+        return cont[idx]
     else:
-        term1 = resolve(content[idx][0], names, content, operations)
-        term2 = resolve(content[idx][1], names, content, operations)
-        if operations[idx] == '+':
-            content[idx] = term1 + term2
-            return term1 + term2
-        elif operations[idx] == '-':
-            content[idx] = term1 - term2
-            return term1 - term2
-        elif operations[idx] == '*':
-            content[idx] = term1 * term2
-            return term1 * term2
-        elif operations[idx] == '/':
-            content[idx] = term1 / term2
-            return term1 / term2
+        term1 = resolve(cont[idx][0], names, cont, ops)
+        term2 = resolve(cont[idx][1], names, cont, ops)
+        result = 0
+        if ops[idx] == '+':
+            result = term1 + term2
+        elif ops[idx] == '-':
+            result = term1 - term2
+        elif ops[idx] == '*':
+            result = term1 * term2
+        elif ops[idx] == '/':
+            result = term1 / term2
+        cont[idx] = result
+        ops[idx] = ''
+        return result
 
 
 if __name__ == '__main__':
@@ -34,6 +34,27 @@ if __name__ == '__main__':
             content.append([line[6:10], line[13:17]])
             operations.append(line[11])
 
-    print(resolve("root", names, content, operations))
+    firstName = content[names.index("root")][0]
+    secondName = content[names.index("root")][1]
+    lower = 0
+    higher = -1
+    mod = 1
+    while True:
+        content[names.index("humn")] = lower + mod
+        #print(lower + mod, end=": ")
+        first = resolve(firstName, names, content.copy(), operations.copy())
+        #print(first, end=" ")
+        second = resolve(secondName, names, content.copy(), operations.copy())
+        #print(second)
+        if first == second:
+            print("solution: " + str(lower + mod))
+            break
+        if first > second:
+            mod *= 2
+        elif first < second:
+            #print("------------------------------------------------")
+            lower = lower + (mod / 2)
+            higher = lower + mod
+            mod = 1
 
     inputFile.close()
