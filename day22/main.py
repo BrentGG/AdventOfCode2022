4e9825194288cbb9e1061874e4cdbf88cf671609
brent <brent.gerets@gmail.com>
2022-12-24 17:10:06 +0100

    day 22 part 2 completed

--- day22/main.py
+++ day22/main.py
@@ -6,54 +6,111 @@ def rotate(facing, rotation):
     return facing
 
 
+def getSection(row, col):
+    if 0 <= row < 50:
+        if 50 <= col < 100:
+            return 1
+        elif 100 <= col < 150:
+            return 2
+    elif 50 <= row < 100:
+        if 50 <= col < 100:
+            return 3
+    elif 100 <= row < 150:
+        if 0 <= col < 50:
+            return 4
+        elif 50 <= col < 100:
+            return 5
+    elif 150 <= row < 200:
+        if 0 <= col < 50:
+            return 6
+    return 0
+
+
 def move(row, col, mapp, facing, amount):
     while amount > 0:
+        oldRow, oldCol, oldFacing = row, col, facing
+        currentSection = getSection(row, col)
         if facing == 0:
             col += 1
-            if col >= len(mapp[row]) or mapp[row][col] == 2:
-                for i in range(len(mapp[row])):
-                    if mapp[row][i] == 0:
-                        col = i
-                        break
-                    elif mapp[row][i] == 1:
-                        return row, col - 1
-            elif mapp[row][col] == 1:
-                return row, col - 1
         elif facing == 1:
             row += 1
-            if row >= len(mapp) or mapp[row][col] == 2:
-                for i in range(len(mapp)):
-                    if mapp[i][col] == 0:
-                        row = i
-                        break
-                    elif mapp[i][col] == 1:
-                        return row - 1, col
-            elif mapp[row][col] == 1:
-                return row - 1, col
         elif facing == 2:
             col -= 1
-            if col < 0 or mapp[row][col] == 2:
-                for i in range(len(mapp[row]) - 1, -1, -1):
-                    if mapp[row][i] == 0:
-                        col = i
-                        break
-                    elif mapp[row][i] == 1:
-                        return row, col - 1
-            elif mapp[row][col] == 1:
-                return row, col + 1
-        else:
+        elif facing == 3:
             row -= 1
-            if row < 0 or mapp[row][col] == 2:
-                for i in range(len(mapp) - 1, -1, -1):
-                    if mapp[i][col] == 0:
-                        row = i
-                        break
-                    elif mapp[i][col] == 1:
-                        return row + 1, col
-            elif mapp[row][col] == 1:
-                return row + 1, col
+        newSection = getSection(row, col)
+
+        if currentSection != newSection:
+            if currentSection == 1:
+                if newSection == 0:
+                    if facing == 2:  # go from the left to the left of 4
+                        facing = 0
+                        row = 149 - oldRow
+                        col = 0
+                    elif facing == 3:  # go from the top to the left of 6
+                        facing = 0
+                        row = 150 + (oldCol - 50)
+                        col = 0
+            elif currentSection == 2:
+                if newSection == 0:
+                    if facing == 0:  # go from the right to the right of 5
+                        facing = 2
+                        row = 149 - oldRow
+                        col = 99
+                    elif facing == 1:  # go from the bottom to the right of 3
+                        facing = 2
+                        row = 50 + (oldCol - 100)
+                        col = 99
+                    elif facing == 3:  # go from the top to the bottom of 6
+                        row = 199
+                        col = oldCol - 100
+            elif currentSection == 3:
+                if newSection == 0:
+                    if facing == 0:  # go from the right to the bottom of 2
+                        facing = 3
+                        row = 49
+                        col = 100 + (oldRow - 50)
+                    elif facing == 2:  # go from the left to the top of 4
+                        facing = 1
+                        row = 100
+                        col = oldRow - 50
+            elif currentSection == 4:
+                if newSection == 0:
+                    if facing == 2:  # go from the left to the left of 1
+                        facing = 0
+                        row = 49 - (oldRow - 100)
+                        col = 50
+                    elif facing == 3:  # go from the top to the left of 3
+                        facing = 0
+                        row = 50 + oldCol
+                        col = 50
+            elif currentSection == 5:
+                if newSection == 0:
+                    if facing == 0:  # go from the right to the right of 2
+                        facing = 2
+                        row = 49 - (oldRow - 100)
+                        col = 149
+                    elif facing == 1:  # go from the bottom to the right of 6
+                        facing = 2
+                        row = 150 + (oldCol - 50)
+                        col = 49
+            elif currentSection == 6:
+                if newSection == 0:
+                    if facing == 0:  # go from the right to the bottom of 5
+                        facing = 3
+                        row = 149
+                        col = 50 + (oldRow - 150)
+                    elif facing == 1:  # go from the bottom to the top of 2
+                        row = 0
+                        col = oldCol + 100
+                    elif facing == 2:  # go from the left to the top of 1
+                        facing = 1
+                        row = 0
+                        col = 50 + (oldRow - 150)
+        if mapp[row][col] == 1:
+            return oldRow, oldCol, oldFacing
         amount -= 1
-    return row, col
+    return row, col, facing
 
 
 def printMap(mapp, row, col, facing):
@@ -82,7 +139,7 @@ def printMap(mapp, row, col, facing):
         print()
     print()
 
-test = 0
+
 if __name__ == '__main__':
     inputFile = open("input.txt", "r")
 
@@ -134,7 +191,7 @@ if __name__ == '__main__':
         if p == 'R' or p == 'L':
             facing = rotate(facing, p)
         else:
-            row, col = move(row, col, mapp, facing, p)
+            row, col, facing = move(row, col, mapp, facing, p)
         #printMap(mapp, row, col, facing)
 
     print(1000 * (row + 1) + 4 * (col + 1) + facing)
